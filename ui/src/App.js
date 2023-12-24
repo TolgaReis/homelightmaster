8b2aa532863d68c70e70e06a5f4a1dbfe88566a3
TolgaReis <tolgaareis@gmail.com>
2023-12-24 09:34:46 +0300

    replace terraform with docker compose

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
+import React, { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
+import MqttService from './service/MqttService';
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const mqttService = new MqttService();
+
+  useEffect(() => {
+    // İstediğiniz MQTT konusunu burada belirtin
+    const topic = 'topic/veri';
+
+    mqttService.connect(topic, () => {
+      // Bağlantı başarılı olduğunda yapılacak işlemleri buraya ekleyebilirsiniz
+    });
+
+    return () => {
+      mqttService.disconnect();
+    };
+  }, []);
+
+  const chartData = {
+    labels: data.map((_, index) => index),
+    datasets: [
+      {
+        label: 'MQTT Verisi',
+        data: data,
+        borderWidth: 1,
+        fill: false,
+      },
+    ],
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Line data={chartData} />
     </div>
   );
-}
+};
 
 export default App;
