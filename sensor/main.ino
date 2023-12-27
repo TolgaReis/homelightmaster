25c67d916dd472ce8d06fcbb536830f6fd31162a
TolgaReis <tolgaareis@gmail.com>
2023-12-27 11:18:59 +0300

    read LDR sensor

--- sensor/main.ino
+++ sensor/main.ino
@@ -1,33 +1,56 @@
-const int trigPin = 9;
-const int echoPin = 10;
+// Distance sensor pins
+const int trigPin = 9;  // Trig
+const int echoPin = 10; // Echo
+
+// Light sensor pin
+const int ldrPin = A0; // analog pin that LDR sensor connected
+
+const int soundSpeed = 0.034;
 
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(9600); // Start the serial communication
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
 }
 
 void loop()
 {
-    // Calculate the distance
+
+    // (speed of sound 340 m/s)
+    int distance = calculateDistance();
+
+    // Read the LDR sensor
+    int ldrValue = readLightDependentResistence();
+
+    // Print the distance and LDR to the monito
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.print(" cm\t");
+
+    Serial.print("LDR: ");
+    Serial.println(ldrValue);
+
+    // Wait
+    delay(1000);
+}
+
+int calculateDistance()
+{
+    // Distance calculation
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
 
-    // Calculate sound wave response time
+    // Response time of sound wave
     long duration = pulseIn(echoPin, HIGH);
 
-    // Calculate the distance (speed of sound 340 m/s)
-    int distance = duration * 0.034 / 2;
+    return duration * soundSpeed / 2;
+}
 
-    // Print the distance
-    Serial.print("Mesafe: ");
-    Serial.print(distance);
-    Serial.println(" cm");
-
-    // wait
-    delay(1000);
+int readLightDependentResistence()
+{
+    return analogRead(ldrPin);
 }
